3c9cbf0c8ea808ebaa3dbf90bd1e6b5fb06bd4c9
engalar <engalar@gmail.com>
2023-02-17 13:58:42 +0800

    Initial commit

---
+++ src/store/index.ts
@@ -0,0 +1,49 @@
+import { configure, makeObservable, when } from "mobx";
+import { CascaderContainerProps } from "../../typings/CascaderProps";
+
+configure({ enforceActions: "observed", isolateGlobalState: true, useProxies: "never" });
+
+export class Store {
+    sub?: mx.Subscription;
+    /**
+     * dispose
+     */
+    public dispose() {}
+
+    constructor(public mxOption: CascaderContainerProps) {
+        makeObservable(this, {});
+
+        when(
+            () => !!this.mxOption.mxObject,
+            () => {
+                this.update();
+
+                this.sub = mx.data.subscribe(
+                    {
+                        guid: this.mxOption.mxObject!.getGuid(),
+                        callback: () => {
+                            this.update();
+                            //等待视图刷新
+                            setTimeout(() => {
+                                this.drawSelection();
+                            }, 1);
+                        }
+                    },
+                    //@ts-ignore
+                    this.mxOption.mxform
+                );
+            },
+            {
+                onError(e) {
+                    console.error(e);
+                }
+            }
+        );
+    }
+    update() {
+        throw new Error("Method not implemented.");
+    }
+    drawSelection() {
+        throw new Error("Method not implemented.");
+    }
+}
---
+++ src/store/objects/BaseMxObject.ts
@@ -0,0 +1,22 @@
+export class BaseMxObject {
+    sub?: mx.Subscription;
+    constructor(public guid: string) {
+        this.sub = mx.data.subscribe({
+            guid: guid,
+            callback: guid => {
+                //@ts-ignore
+                this.onChange(guid);
+            }
+        });
+    }
+    get mxObject(): mendix.lib.MxObject | undefined {
+        return mx.data.getCachedObject(this.guid);
+    }
+    public dispose(): void {
+        if (this.sub) {
+            mx.data.unsubscribe(this.sub);
+            this.sub = undefined;
+        }
+    }
+    onChange?: (guid: string) => void;
+}
